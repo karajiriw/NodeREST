3cbb170a790b1cc63be83f2fb6a9e169ca64e013
karajiriw <koki18062547@gmail.com>
2024-03-06 10:13:56 +0700

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+//const cors = require("cors");
 
 mongoose.connect(
     "mongodb://admin:VZQlaa25848@node57049-kittipong-noderest.proen.app.ruk-com.cloud:11835",
@@ -23,7 +23,7 @@ const book = mongoose.model("Book", {
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
 
 app.post("/books", async (req, res) => {
     try{
